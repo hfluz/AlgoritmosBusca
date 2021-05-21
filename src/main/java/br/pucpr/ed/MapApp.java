94a2ac2b60637b33e329b78a7223f9b26c8e26b7
Humberto Ferreira da Luz Junior <hfluz@uel.br>
2021-05-20 21:48:29 -0300

    Correção na classe MapApp.

--- src/main/java/br/pucpr/ed/MapApp.java
+++ src/main/java/br/pucpr/ed/MapApp.java
@@ -1,8 +1,8 @@
 package br.pucpr.ed;
 
+import br.pucpr.ed.map.HashMap;
 import br.pucpr.ed.map.Map;
 import br.pucpr.ed.map.OrderedMap;
-import java.util.HashMap;
 
 public class MapApp {
 
@@ -19,16 +19,16 @@ public class MapApp {
         System.out.println(myMap.values());
         System.out.println(myMap.entrySet());
 
-        java.util.Map<String, Integer> hashMap = new HashMap<>(50);
-        myMap.put("map", 1);
-        myMap.put("test", 4);
-        myMap.put("pom.xml", 2);
-        myMap.put("java", 5);
-        test = myMap.remove("test");
-        System.out.println(myMap.get("map"));
-        System.out.println(myMap.get("teste"));
-        System.out.println(myMap.keySet());
-        System.out.println(myMap.values());
-        System.out.println(myMap.entrySet());
+        Map<String, Integer> hashMap = new HashMap<>(50);
+        hashMap.put("map", 1);
+        hashMap.put("test", 4);
+        hashMap.put("pom.xml", 2);
+        hashMap.put("java", 5);
+        test = hashMap.remove("test");
+        System.out.println(hashMap.get("map"));
+        System.out.println(hashMap.get("teste"));
+        System.out.println(hashMap.keySet());
+        System.out.println(hashMap.values());
+        System.out.println(hashMap.entrySet());
     }
 }
